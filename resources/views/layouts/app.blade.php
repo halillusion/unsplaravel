9bed87a344eca5d3edba1995564123ed886ce2ae
halillusion <halillusion@gmail.com>
2022-09-16 01:23:01 +0300

    layout fixes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,38 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title') | Unsplaravel</title>
+        <title>@yield('title') — Unsplaravel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
-            @yield('content')
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Unsp<strong>laravel</strong></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menu">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main>
+            <section class="py-5 text-center container">
+                <div class="row py-lg-5">
+                    <div class="col-lg-6 col-md-8 mx-auto">
+                        <h1 class="fw-light">A sample Unsplash API Client</h1>
+                        <p class="lead text-muted">It is a sample project that presents the content provided by the Unsplash API. It adds 30 random photos to its database by querying once a day.</p>
+                    </div>
+                </div>
+            </section>
+            <div class="container">
+                @yield('content')
+            </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
