c068822b08f2b716fefa0fba463c5fa63875f94a
halillusion <halillusion@gmail.com>
2022-09-16 01:23:09 +0300

    home blade fixes.

--- resources/views/unsplash/home.blade.php
+++ resources/views/unsplash/home.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 @section('title', 'Home')
 @section('content')
-    @foreach ($photos as $photo)
-        <p>This is user {{ $photo->artist->name }}</p>
-    @endforeach
+    <div class="row">
+        @foreach ($photos as $photo)
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="card shadow-sm">
+                    <img class="card-img-top" width="100%" height="225" src="{{ $photo->pivot->small_url }}" alt="{{ $photo->description }}" />
+                    <div class="card-body">
+                        <p class="card-text">{{ $photo->description }}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="{{ url('/photo/' . $photo->photo_id ) }}" class="btn btn-sm btn-outline-secondary">View</a>
+                            </div>
+                            <small class="text-muted">by {{ '@'.$photo->artist->username }}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
